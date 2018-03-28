c9c9ae09a275ff3a989e3fa0f273a350e1ddcc8a
Aidas <balcaitis.aidas@gmail.com>
2018-03-28 23:17:23 +0300

    Added Tour Service with DTO

---
+++ aspnet-core/src/Skautatinklis.Web.Core/DTOs/TourDto.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Abp.AutoMapper;
+using Skautatinklis.Models;
+
+namespace Skautatinklis.DTOs
+{
+    [AutoMapTo(typeof(Tour))]
+    [AutoMapFrom(typeof(Tour))]
+    public class TourDto
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(2550)]
+        public string Description { get; set; }
+
+        [Required]
+        public int TotalPoints { get; set; }
+
+        [Required]
+        public int TimeToEnterAnswersInSeconds { get; set; }
+
+        [Required]
+        public int OrderNumber { get; set; }
+
+        public int QuestionsCount { get; set; }
+
+        public bool IsLastTour { get; set; } = false;
+    }
+}
---
+++ aspnet-core/src/Skautatinklis.Web.Core/Services/TourService/ITourService.cs
@@ -0,0 +1,8 @@
+﻿using Abp.Application.Services;
+
+namespace Skautatinklis.Services.TourService
+{
+    public interface ITourService : IApplicationService
+    {
+    }
+}
