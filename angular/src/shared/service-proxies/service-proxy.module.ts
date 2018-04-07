e0ec6515121da12c57f20b58c3301c20aa4b00f3
Aidas <balcaitis.aidas@gmail.com>
2018-04-07 14:51:31 +0300

    Added service proxies

--- angular/src/shared/service-proxies/service-proxy.module.ts
+++ angular/src/shared/service-proxies/service-proxy.module.ts
@@ -10,7 +10,15 @@ import * as ApiServiceProxies from './service-proxies';
         ApiServiceProxies.UserServiceProxy,
         ApiServiceProxies.TokenAuthServiceProxy,
         ApiServiceProxies.AccountServiceProxy,
-        ApiServiceProxies.ConfigurationServiceProxy
+        ApiServiceProxies.ConfigurationServiceProxy,
+		ApiServiceProxies.MindfightServiceProxy,
+		ApiServiceProxies.PlayerServiceProxy,
+		ApiServiceProxies.QuestionServiceProxy,
+		ApiServiceProxies.RegistrationServiceProxy,
+		ApiServiceProxies.ResultServiceProxy,
+		ApiServiceProxies.TeamAnswerServiceProxy,
+		ApiServiceProxies.TeamServiceProxy,
+		ApiServiceProxies.TourServiceProxy
     ]
 })
 export class ServiceProxyModule { }
