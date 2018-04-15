e1a9b96f7e2ee5285e6dec1c621c767e210fbcee
Aidas <balcaitis.aidas@gmail.com>
2018-04-15 22:59:57 +0300

    Added team component

---
+++ angular/src/app/team/team.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, Injector } from '@angular/core';
+import { AppComponentBase } from '@shared/app-component-base';
+import { TeamServiceProxy, TeamDto, PlayerDto, PlayerServiceProxy, TeamPlayerDto } from 'shared/service-proxies/service-proxies';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+    selector: 'app-team',
+    templateUrl: './team.component.html',
+    styleUrls: ['./team.component.css']
+})
+export class TeamComponent extends AppComponentBase implements OnInit {
+    teamId: number;
+    team: TeamDto;
+    teamPlayers: TeamPlayerDto[] = [];
+    teamLeader: TeamPlayerDto;
+    activeTeamPlayers: TeamPlayerDto[] = [];
+    passiveTeamPlayers: TeamPlayerDto[] = [];
+    playerInfo: PlayerDto;
+
+    constructor(
+        injector: Injector,
+        private teamService: TeamServiceProxy,
+        private playerService: PlayerServiceProxy,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+        super(injector);
+    }
+
+    ngOnInit() {
+        this.getPlayerInfo();
+    }
+
+    getPlayerInfo(): void {
+        this.playerService.getPlayerInfo(abp.session.userId).subscribe((result) => {
+            this.playerInfo = result;
+            console.log(this.playerInfo);
+            if (this.playerInfo.teamId != 0) {
+                this.getPlayerTeam(this.playerInfo.teamId);
+            }
+        });
+    };
+
+    getPlayerTeam(teamId): void {
+        this.teamService.getTeam(teamId).subscribe(
+            (result) => {
+                this.team = result;
+                this.getAllTeamPlayers(this.playerInfo.teamId);
+                console.log(this.team);
+            }
+        );
+    }
+
+    getAllTeamPlayers(teamId): void {
+        this.teamService.getAllTeamPlayers(teamId).subscribe(
+            (result) => {
+                this.teamPlayers = result;
+                console.log(this.teamPlayers);
+                this.getActivePassiveTeamPlayers(this.teamPlayers);
+            }
+        );
+    }
+
+    getActivePassiveTeamPlayers(teamPlayers): void {
+        var that = this;
+        teamPlayers.forEach(function (player) {
+            if (player.id === that.team.leaderId) {
+                that.teamLeader = player;
+            } else if (player.isActiveInTeam) {
+                that.activeTeamPlayers.push(player);
+            } else {
+                that.passiveTeamPlayers.push(player);
+            }
+        });
+    }
+}
